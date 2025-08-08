6d36842dda45837b8d9350a390ff670338252d38
kuansy373 <kuanfugongwu@gmail.com>
2025-08-08 09:24:43 +0900

    Update color-tool-080125.js

--- color-tool-080125.js
+++ color-tool-080125.js
@@ -135,7 +135,7 @@
       </div>
       <div class="row contrast-row" style="align-items: center;">
         <strong>Contrast:</strong>
-        <span id="contrastRatio" style="width: 45px;">-</span>
+        <span id="contrastRatio" style="width: 51px;">-</span>
         <input id="contrastMin" class="hex-display" style="width: 50px;" type="number" min="1" max="21" step="0.1" value="3" title="Minimum contrast ratio">
         <span style="margin: 0;">–</span>
         <input id="contrastMax" class="hex-display" style="width: 50px;" type="number" min="1" max="21" step="0.1" value="21" title="Maximum contrast ratio">
