ec76cda3b515d5d1b9bd339efb9c59df7792c8f4
kuansy373 <kuanfugongwu@gmail.com>
2025-08-22 16:40:39 +0900

    Update color-tool-080125.js

--- color-tool-080125.js
+++ color-tool-080125.js
@@ -473,7 +473,8 @@
       updateContrast();
       window.__pickrLoaded = !1
     }
-  }).catch((err) => {
+  })
+    .catch((err) => {
     alert("Pickr の読み込みに失敗しました。CSP によってブロックされている可能性があります。");
     console.error("Pickr load error:", err);
 });
