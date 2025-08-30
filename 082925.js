92e69c34fcc915e16a06a5879d96173c782916f8
kuansy373 <kuansy373@gmail.com>
2025-08-30 09:57:08 +0900

    スタイル調整

--- 082925.js
+++ 082925.js
@@ -271,11 +271,13 @@
       }
 
       #pickrClose {
+        all: initial;
+        font-size: 15px;
+        font-weight: bolder;
         cursor: pointer;
         position: absolute;
         top: 5px;
         right: 7px;
-        font-weight: 100;
       }
 
       #pickrContainer .row.contrast-row {
@@ -320,11 +322,13 @@
         all: initial;
         font-family: monospace;
         font-size: 14px;
+        font-weight: normal;
         padding: 2px 4px;
         background: #fff;
         border: 1px solid #ccc;
         border-radius: 4px;
-        text-align: center;
+        letter-spacing: 0.7px;
+        text-align: left;
         width: 80px;
       }
 
@@ -333,15 +337,29 @@
         cursor: pointer;
         padding: 2px 2px;
         font-size: 1em;
+        font-weight: bolder;
         border: 1px solid #aaa;
         background: #e0e0e0;
         border-radius: 4px;
+      }
+
+      .switch-bgfg {
+        all: initial;
+        font-family: monospace;
+        font-size: 18px;
+        border: 1px solid #aaa;
+        background: #e0e0e0;
+        border-radius: 4px;
+        width: 19px;
+        height: 25px;
+        text-align: center;
       }
 
       input.contrast-display {
         all: initial;
         font-family: monospace;
         font-size: 14px;
+        font-weight: normal;
         width: 40px;
         padding: 1px;
         background: #ffffff;
@@ -425,7 +443,7 @@
         height: 20px !important;
         margin-top: 10px !important;
         font-family: monospace !important;
-        font-size: 18px !important;
+        font-size: 15px !important;
       }
 
       .pcr-save {
@@ -468,7 +486,7 @@
         </div>
         <button id="fgHexLoad" class="hex-load-btn">⇦</button>
         <input id="fgHex" class="hex-display" value="-">
-        <button id="swapColorsBtn" class="hex-load-btn" style="font-family: monospace;font-size: 15px;">↕</button>
+        <button id="swapColorsBtn" class="switch-bgfg">↕</button>
       </div>
     
       <div class="row">
