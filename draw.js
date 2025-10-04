98d583754fdbcd1d28310727f4e50266590bb029
kuansy373 <kuansy373@gmail.com>
2025-10-04 13:47:20 +0900

    消しゴムを追加

--- draw.js
+++ draw.js
@@ -5,27 +5,50 @@ javascript:(function(){
   c.style.position='fixed';
   c.style.top='0';
   c.style.left='0';
-  c.style.zIndex='1';
+  c.style.zIndex='1000';
   c.style.cursor='crosshair';
   document.body.appendChild(c);
   var ctx=c.getContext('2d');
   ctx.strokeStyle='black';
   ctx.lineWidth=3;
   var drawing=false;
+  var mode='pen'; // 'pen' or 'eraser'
 
-  // マウス用イベント
+  // UIコンテナ作成
+  var container=document.createElement('div');
+  container.style.position='fixed';
+  container.style.top='10px';
+  container.style.left='10px';
+  container.style.zIndex='1001';
+  container.style.background='rgba(255,255,255,0.8)';
+  container.style.padding='5px';
+  container.style.border='1px solid #ccc';
+  container.style.borderRadius='5px';
+  
+  var penBtn=document.createElement('button');
+  penBtn.innerText='ペン';
+  var eraserBtn=document.createElement('button');
+  eraserBtn.innerText='消しゴム';
+  container.appendChild(penBtn);
+  container.appendChild(eraserBtn);
+  document.body.appendChild(container);
+
+  penBtn.onclick=function(){mode='pen'; ctx.globalCompositeOperation='source-over'; ctx.strokeStyle='black';};
+  eraserBtn.onclick=function(){mode='eraser'; ctx.globalCompositeOperation='destination-out'; ctx.strokeStyle='rgba(0,0,0,1)';};
+
+  // マウスイベント
   c.addEventListener('mousedown',function(e){
-    drawing=true; 
-    ctx.beginPath(); 
+    drawing=true;
+    ctx.beginPath();
     ctx.moveTo(e.clientX,e.clientY);
   });
   c.addEventListener('mousemove',function(e){
-    if(drawing){ctx.lineTo(e.clientX,e.clientY); ctx.stroke();}
+    if(drawing){ ctx.lineTo(e.clientX,e.clientY); ctx.stroke(); }
   });
   c.addEventListener('mouseup',function(){drawing=false;});
   c.addEventListener('mouseleave',function(){drawing=false;});
 
-  // タッチ用イベント
+  // タッチイベント
   c.addEventListener('touchstart',function(e){
     drawing=true;
     var touch=e.touches[0];
