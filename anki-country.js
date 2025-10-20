458b9dcc0aa3c5b46bc029ab61ad2dc5632e26e4
kuansy373 <kuansy373@gmail.com>
2025-10-21 01:45:43 +0900

    座標微調整

--- anki-country.js
+++ anki-country.js
@@ -785,7 +785,7 @@ javascript:(function () {
         'Europe': { center: [14, 52], zoom: 2.7 },
         'Africa': { center: [17, 5], zoom: 2.4 },
         'MiddleEast': { center: [54, 36], zoom: 2.7 },
-        'Asia': { center: [110, 25], zoom: 2.5 },
+        'Asia': { center: [105, 25], zoom: 2.5 },
         'Oceania': { center: [147, -25], zoom: 2.5 },
         'North America': { center: [-85, 25], zoom: 3 },
         'South America': { center: [-60, -18], zoom: 2.4 },
