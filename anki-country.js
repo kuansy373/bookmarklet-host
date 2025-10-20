c0de2f2d17c8049b93495e9908be5a6bcb1e5b96
kuansy373 <kuansy373@gmail.com>
2025-10-21 01:37:24 +0900

    座標微調整

--- anki-country.js
+++ anki-country.js
@@ -782,13 +782,13 @@ javascript:(function () {
 
       // 地域ごとの中心座標とズーム設定
       var regionView = {
-        'Europe': { center: [10, 50], zoom: 2.7 },
-        'Africa': { center: [15, 5], zoom: 2.4 },
-        'MiddleEast': { center: [50, 35], zoom: 2.7 },
-        'Asia': { center: [110, 25], zoom: 2.2 },
+        'Europe': { center: [14, 52], zoom: 2.7 },
+        'Africa': { center: [17, 5], zoom: 2.4 },
+        'MiddleEast': { center: [54, 36], zoom: 2.7 },
+        'Asia': { center: [110, 25], zoom: 2.5 },
         'Oceania': { center: [147, -25], zoom: 2.5 },
         'North America': { center: [-85, 25], zoom: 3 },
-        'South America': { center: [-60, -21], zoom: 2.3 },
+        'South America': { center: [-60, -18], zoom: 2.4 },
       };
       
       // 地域名クリックでフォーカスズーム
