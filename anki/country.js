a008b98e860283205f4fa5ec1e2f07d5bfd91f50
kuansy373 <kuansy373@gmail.com>
2025-11-07 10:23:12 +0900

    Update country.js

--- anki/country.js
+++ anki/country.js
@@ -75,7 +75,7 @@ javascript:(function () {
     var geoUrls = {
       world: 'https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson',
       usaStates: 'https://raw.githubusercontent.com/datasets/geo-admin1-us/master/data/admin1-us.geojson',
-      capitals: 'https://raw.githubusercontent.com/kuansy373/bookmarklet-host/main/capitals.geojson'
+      capitals: 'https://raw.githubusercontent.com/kuansy373/bookmarklet-host/main/data/capitals.geojson'
     };
 
     // 地域別カラー設定
