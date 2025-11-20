0d536b29f069668c4cdd162be47797a2c645e6c9
kuansy373 <kuansy373@gmail.com>
2025-11-21 03:42:03 +0900

    400文字チャンク

--- vertical-read-080825.js
+++ vertical-read-080825.js
@@ -266,8 +266,8 @@ let text = '';
         
         partHTML = `<span style="opacity:0.5;">${beforeOverlap}</span>${afterOverlap}`;
     }
-    // partHTML を 700文字ごとの小チャンクに分割
-    const smallChunks = chunkHTMLSafe(partHTML, 700);
+    // partHTML を 400文字ごとの小チャンクに分割
+    const smallChunks = chunkHTMLSafe(partHTML, 400);
     parts.push(smallChunks); // parts[ページ番号][小チャンク番号]
     prevEndVisiblePos = endVisiblePos;
     
